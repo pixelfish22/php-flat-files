54bb5279ff315f8e3a31ee0591a783a911920d8b
Ken Korth <pixelfish22@gmail.com>
2015-07-30 02:18:17 +0000

    read is operational

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-
+<?php 
+$file = fopen("black_night.txt", "r") or die("Unable to open file!");
+$file_data = fread($file,filesize("black_night.txt"));
+fclose($file);
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Flat File Mini Assignment</title>
     </head>
     <body>
-        <h1>Display Your text flat file below</h1>
+        <h1>The Black Knight</h1>
+        <pre><?php echo $file_data; ?></pre>
     </body>
 </html>
