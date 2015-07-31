d401ae4136a3284e9fb4e23961badb9360b8dd02
Ken Korth <ken@kenkorth.com>
2015-07-30 19:22:20 -0500

    fread trest

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-
+<?php 
+$file_ref = fopen("black_knight.txt", "r");
+$file_data = fread($file_ref, 500);
+fclose($file_ref);
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Flat File Mini Assignment</title>
     </head>
     <body>
-        <h1>Display Your text flat file below</h1>
+        <h1>Demo</h1>
+        <pre> <?php echo $file_data; ?> </pre>
     </body>
 </html>
