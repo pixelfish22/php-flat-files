51f55f9c596fe629d2c4ff7b443d9129fbf4deca
Ken Korth <ken@kenkorth.com>
2015-07-30 19:33:19 -0500

    this thing

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
-$file_ref = fopen("black_knight.txt", "r");
-$file_data = fread($file_ref, 500);
+$file_ref = fopen("black_knight.txt", "r") or die("I just died");
+$file_data = fread($file_ref, filesize("black_knight.txt"));
 fclose($file_ref);
 ?>
 <!DOCTYPE html>
@@ -10,6 +10,11 @@ fclose($file_ref);
     </head>
     <body>
         <h1>Demo</h1>
+        <form action="">
+        	<label> What you wanna say!</label>
+        	<input type="text" name="new_line">
+        	<button type="submit">Yup!</button>
+    	</form>
         <pre> <?php echo $file_data; ?> </pre>
     </body>
 </html>
